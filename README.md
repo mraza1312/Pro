d8c9727b697ccef13af5fa9f379f59fee2b5d7ae
mraza1312 <iammarco1995@gmail.com>
2018-12-15 17:33:47 +0530

    some changes made

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-#Assignment:The Unix Workbench 
+# Assignment:The Unix Workbench 
+
+this makefile last ran on: 
+12/15/2018, 05:33:25 PM
 
 the file *guessing game* have following no of entries: 
-26
+29
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
-# this 
 #echo "Hello this is a guessing game where you guess no of files in a directory."
 
 # it was created as a part of an assignment from the mooc:The Unix Workbench
- 
-chmod +x guessinggame.sh
+
+fno=$(ls | wc -l)
 
 echo "hello there! Can you guess how many files are there in this
 directory?"
 
-while true; do
+while true;
+do
 read no
 
-if (("$no" == "3")) ; then break ;
-elif(( "$no" > "3")); then echo "your guess is high! try again." 
-elif(("$no" < "3")); then echo "your guess is low! try again."  
+if (("$no" == "$fno" )) ; then
+	 break ;
+
+elif(( "$no" > "$fno" )); then
+	 echo "your guess is high! try again." 
+
+elif(("$no" < "$fno" )); then 
+	echo "your guess is low! try again."  
 
 fi
-
 done
 
 echo "congratulations! you pass"
-
 
 exit 1  #exiting the script
